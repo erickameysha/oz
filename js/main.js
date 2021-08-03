d6d817d40ede027f4adc71fb84d2b8492feccbf4
Eric <ekameyshaaa@gmail.com>
2021-08-03 12:23:00 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(function () {
+  $(".menu a, .footer__shape").on("click",  function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+ 
+  $('.slider-blog__inner').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-right.svg" alt=""></button>',
+    nextArrow:'<button type="button" class="slick-next"><img src="images/arrow-left.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false
+        }
+      },
+      
+    ]
+    
+});
+$('.menu__btn, .menu a').on('click', function(){
+  $('.header__top-inner').toggleClass('header__top-inner--active');
+  
+}); 
+var mixer = mixitup('.portfolio__content');
+
+});
